05dc8cb3b17a8da44dbfa87222bcb51071940b9b
gijsboddeus <gijs@afosto.io>
2022-03-15 14:27:50 +0100

    fix: add new lines at end of files

--- src/hooks/useClientSetup.js
+++ src/hooks/useClientSetup.js
@@ -7,7 +7,63 @@ const useClientSetup = (key) => {
 
   const fetchSettings = useCallback(async () => {
     const settings = await client.getSettings();
-    setSettings(settings);
+    const formattedSettings = {
+      ...settings,
+      filters: {
+        show_for: 'test-producten-be-csv',
+        sorting: [
+          'category',
+          'price'
+        ],
+        options_count: 5,
+        max_options_count: 25
+      },
+      is_autocomplete: true,
+      click_tracking: {
+        is_enabled: true,
+        url: 'string'
+      },
+      indexes: [
+        {
+          alias: 'test-producten-be-csv',
+          template: 'PRODUCTS',
+          results_per_page: 12,
+          template_mapping: {
+
+          },
+          filters: [
+            {
+              key: 'merk',
+              label: 'Brand',
+              type: 'STRING',
+              options_count: 10,
+              max_options_count: 25
+            },
+            {
+              key: 'sku',
+              label: 'SKU',
+              type: 'STRING',
+              options_count: 10,
+              max_options_count: 25
+            },
+            {
+              key: 'price',
+              label: 'Price',
+              type: 'NUMBER',
+            },
+          ]
+        },
+        {
+          alias: 'paginas-test',
+          template: 'PAGES',
+          results_per_page: 12,
+          template_mapping: {
+
+          },
+        }
+      ]
+    }
+    setSettings(formattedSettings);
   }, [client]);
 
   useEffect(() => {
