3d3a3fb4b7200ed3ab550cd2f3e549bb4901138a
gijsboddeus <gijs@afosto.io>
2022-03-28 15:05:08 +0200

    feat: added custom ScrollTo component to restore scroll after pagination

---
+++ src/hooks/usePrevious.js
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from 'preact/hooks';
+
+const usePrevious = value => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+export default usePrevious;
