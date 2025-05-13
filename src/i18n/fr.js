a91bfd640a5bc71a5b69dabf4d35b5e9ad40dfe5
Gijs Boddeus <gijs@afosto.io>
2025-05-13 15:37:29 +0200

    Feature/french-locale-support
    feat: added french i18n file

---
+++ src/i18n/fr.js
@@ -0,0 +1,24 @@
+const messages = {
+  close: 'Fermer',
+  filters: {
+    closeFilters: 'Fermer',
+    noResults: 'Aucune option',
+    reset: 'Réinitialiser les filtres',
+    showMore: 'Afficher plus',
+    showLess: 'Afficher moins',
+    title: 'Filtres',
+  },
+  hitsPerPage: {
+    optionsLabel: '{value} résultats',
+  },
+  search: {
+    placeholder: 'Rechercher...',
+    resetTitle: 'Effacer votre recherche.',
+    submitTitle: 'Envoyer votre recherche.',
+  },
+  stats: {
+    resultsLabel: '{value} résultats trouvés',
+  },
+};
+
+export default messages;
